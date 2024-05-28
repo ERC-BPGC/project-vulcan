28903fe2648ad350c2ebc0ad09ba68139914a99e
Parth Shah <parth2rock@gmail.com>
2024-05-28 15:47:38 +0530

    Cleaned Auto

--- Auto/old_not_needed/gpt_test/brd.py
+++ Auto/old_not_needed/gpt_test/brd.py
@@ -3,7 +3,7 @@ from Bard import Chatbot
 
 # token = os.environ.get('bard')
 
-token = "WwgUKHp2_WOi3nApv48FZ099HacULOchHt5J-PhvSNHA11tQ7EZ2WIVZlMgn9HUtmh3K_Q."
+token = ""
 
 bot = Chatbot(token)
 
